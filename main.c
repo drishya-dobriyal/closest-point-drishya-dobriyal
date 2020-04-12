cea0804b5cff7e6a5783ace5e83bc42a8b52cb74
Drishya Dobriyal <dobriyaldrishya@gmail.com>
2020-04-12 17:21:28 +0530

    added data to main

--- main.c
+++ main.c
@@ -2,5 +2,10 @@
 
 int main(void)
 {
+  Point food_points[] = {{ 18, 76}, { 19, 66}, { 89, 57}, { 9, 71}, { 55, 38}};
+  Point current_location, closest_food_location;
+  int points_length = sizeof(food_points) / sizeof(food_points[0]);
+  printf("Enter Your co-ordinates :\n");
+  scanf("%d %d",current_location.x, current_location.y );
   return 0;
 }
