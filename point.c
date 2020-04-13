245b1d8bf7c3cb2b93af00849bbb266bfc079365
Drishya Dobriyal <dobriyaldrishya@gmail.com>
2020-04-13 14:55:07 +0530

    modified get_closest_food

--- point.c
+++ point.c
@@ -8,12 +8,14 @@ double get_distance(int x1, int y1, int x2, int y2) {
 void get_closest_food(Point food_points[], int points_length, Point current_location, Point *closest_food_location){
   double closest_distance, distance;
   
+  closest_distance = get_distance(food_points[0].x, food_points[0].y, current_location.x, current_location.y);
+  *closest_food_location = food_points[0];
+  
   for(int i = 0; i < points_length; i++){
     distance = get_distance(FOOD_POINT,CURRENT_LOCATION);
-    if(i == 0 || distance < closest_distance) {
+    if(distance < closest_distance) {
       closest_distance = distance;
-      closest_food_location->x =  food_points[i].x;
-      closest_food_location->y =  food_points[i].y;
+      *closest_food_location = food_points[i];
     }
   }
   return;
