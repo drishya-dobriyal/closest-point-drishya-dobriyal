da41b211e58c626fa29df7df1e8315c819c31118
Drishya Dobriyal <dobriyaldrishya@gmail.com>
2020-04-13 15:33:07 +0530

    modified get_closest_food

--- point.c
+++ point.c
@@ -1,18 +1,20 @@
 #include <math.h>
 #include "point.h"
 
-double get_distance(int x1, int y1, int x2, int y2) {
-  return sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2));
+double get_distance(Point point1,Point point2 ) {
+  int x_distance = point1.x - point2.x;
+  int y_distance = point1.y - point2.y;
+  return sqrt(pow((x_distance), 2) + pow((y_distance), 2));
 }
 
 void get_closest_food(Point food_points[], int points_length, Point current_location, Point *closest_food_location){
   double closest_distance, distance;
-  
-  closest_distance = get_distance(food_points[0].x, food_points[0].y, current_location.x, current_location.y);
+
+  closest_distance = get_distance(food_points[0], current_location );
   *closest_food_location = food_points[0];
   
   for(int i = 0; i < points_length; i++){
-    distance = get_distance(FOOD_POINT,CURRENT_LOCATION);
+    distance = get_distance(food_points[i], current_location );
     if(distance < closest_distance) {
       closest_distance = distance;
       *closest_food_location = food_points[i];
--- point.h
+++ point.h
@@ -1,6 +1,4 @@
 #define FOOD_CO_ORDINATES {{ 18, 76}, { 19, 66}, { 89, 57}, { 9, 71}, { 55, 38}}
-#define FOOD_POINT food_points[i].x, food_points[i].y
-#define CURRENT_LOCATION current_location.x, current_location.y 
 
 typedef struct
 {
@@ -8,5 +6,5 @@ typedef struct
   int y;
 } Point;
 
-double get_distance( int , int , int, int);
+double get_distance( Point, Point);
 void get_closest_food(Point food_points[], int points_length, Point current_location, Point *closest_food_location);
